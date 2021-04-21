e47916b9c17a16b927e223176dcf3d965628fe9b
nginxim <372669319@qq.com>
2021-04-21 10:43:12 +0800

    first commit

---
+++ src/main/java/com/rumenz/controller/DemoController.java
@@ -0,0 +1,25 @@
+package com.rumenz.controller;
+
+import com.rumenz.bean.Demo;
+import com.rumenz.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+
+    @Autowired
+    private DemoService demoService;
+
+    @RequestMapping("/hello/{id}")
+    @ResponseBody
+    public String hello(@PathVariable(value = "id") Long id) {
+        return Optional.ofNullable(demoService.getDemoById(id)).map(Demo::toString).orElse("empty String");
+    }
+}
